4f56a2389d2e7156f7bb083bf1fe666ef1f85ed2
Vatsal Upadhyay <vatsal11042009@gmail.com>
2026-08-27 12:04:08 -0400

    lft-backend java 21 + Maven + SpringBoot setup

---
+++ lft-backend/src/main/java/Lost/Found/Tracker/LftBackendApplication.java
@@ -0,0 +1,13 @@
+package Lost.Found.Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LftBackendApplication.class, args);
+	}
+
+}
---
+++ lft-backend/src/test/java/Lost/Found/Tracker/LftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Lost.Found.Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
